b9066c66692570074d584f968e2798e85cc02022
evgen <evgen@10.230.48.120>
2016-09-19 11:53:33 -0400

    Finished Assignment "LoanCalculator"

--- School/src/dawson112/assignments/LoanCalculator.java
+++ School/src/dawson112/assignments/LoanCalculator.java
@@ -17,15 +17,29 @@ public class LoanCalculator {
 		System.out.println("Enter monthly payments in dollars");
 		double monthlyAmount = reader.nextDouble();
 		
-		//Converts the dollars into cents
-		principle = principle * 100;
-		monthlyAmount = monthlyAmount * 100;
-		
 		//Converts the annual interest rate into monthly interest rate
 		double monthlyInterestRate = interestRate / 1200.0;
 		
-		int amountOfMonths = (int) Math.ceil((Math.log(monthlyAmount / (monthlyAmount - monthlyInterestRate * principle)) / Math.log(1 + monthlyInterestRate)));
+		//Calculates the amount of months needed to pay the loan
+		double amountOfMonths = (Math.log(monthlyAmount / (monthlyAmount - monthlyInterestRate * principle)) / Math.log(1 + monthlyInterestRate));
 		
-		System.out.println(amountOfMonths);
+		//Rounds up the amount of months and calculates the number of years
+		double roundedAmountOfMonths = Math.ceil(amountOfMonths);
+		double numberOfYears = (double) roundedAmountOfMonths / 12;
+		
+		//Calculates the total amount paid
+		double totalAmountPaid = amountOfMonths * monthlyAmount;
+		
+		//Calculates the amount added by the interest
+		double interestAmount = totalAmountPaid - principle;
+		
+		//Prints the results
+		System.out.println("Months to pay off: " + roundedAmountOfMonths);
+		System.out.println("Years to pay off: " + numberOfYears);
+		System.out.println("Total amount paid: " + totalAmountPaid);
+		System.out.println("Total amount in interest: " + interestAmount);
+		
+		//Closes the scanner
+		reader.close();
 	}
 }
